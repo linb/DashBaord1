ba6195b99473bd4030fdbd4e49c175ec51fea324
Jack Li <linb.net@gmail.com>
2020-12-06 22:23:44 -0500

    Updated by CrossUI React Builder

--- pages/App/DashBoard/PageVisitorChart.js
+++ pages/App/DashBoard/PageVisitorChart.js
@@ -1,14 +1,38 @@
 
 /*jshint esversion: 8 */
 import { React, html } from "../../../web_modules/preact-htm/index.js";
-
-const Module = props => {
-  const [defaultState, setDefaultState] = React.useState({});
-
-  return html`
-<${React.Fragment}>
-</${React.Fragment}>
-`;
+import { LineChart } from "../../../web_modules/recharts/index.js";
+import { ResponsiveContainer } from "../../../web_modules/recharts/index.js";
+import { Line } from "../../../web_modules/recharts/index.js";
+import { XAxis } from "../../../web_modules/recharts/index.js";
+import { YAxis } from "../../../web_modules/recharts/index.js";
+import { CartesianGrid } from "../../../web_modules/recharts/index.js";
+import { Tooltip } from "../../../web_modules/recharts/index.js";
+import { Legend } from "../../../web_modules/recharts/index.js";
+export const PageVisitorChart = props => {
+    return html`
+        <${React.Fragment}>
+            <${ResponsiveContainer} key="bd8xf94g">
+                <${LineChart} width=${600} height=${300} data=${recharts_demodata1} margin=${{ "top": 5, "right": 30, "left": 20, "bottom": 5 }}>
+                    <${XAxis} dataKey="name">
+                    </${XAxis}>
+                    <${YAxis}>
+                    </${YAxis}>
+                    <${CartesianGrid} strokeDasharray="3 3">
+                    </${CartesianGrid}>
+                    <${Legend}>
+                    </${Legend}>
+                    <${Line} type="monotone" dataKey="pv" stoke="#ff00ff" activeDot=${{ "r": 6 }}>
+                    </${Line}>
+                    <${Line} type="monotone" dataKey="uv" stoke="#82ca9d" activeDot=${{ "r": 6 }}>
+                    </${Line}>
+                    <${Tooltip}>
+                    </${Tooltip}>
+                </${LineChart}>
+            </${ResponsiveContainer}>
+        </${React.Fragment}>
+    `;
 };
 
-export default Module;
+
+export default PageVisitorChart;
