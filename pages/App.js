86d01f632136ad4e5ec720fbc0dcc65d84b00b5a
Jack Li <linb.net@gmail.com>
2020-12-06 19:53:01 -0500

    Updated by CrossUI React Builder

--- pages/App.js
+++ pages/App.js
@@ -2,20 +2,30 @@
 /*jshint esversion: 8 */
 import { React, html } from "../web_modules/preact-htm/index.js";
 import { useModule } from "../web_modules/react-hook-module/index.js";
+import { If } from "../web_modules/react-hook-module/index.js";
 
 const Module = props => {
-  const [defaultState, setDefaultState] = React.useState({});
-  const {
-    module,
-    router,
-    request,
-    auth
-  } = useModule(props, {});
+    const [defaultState, setDefaultState] = React.useState({
+    });
+    const {
+        module,
+        router,
+        request,
+        auth
+    } = useModule(props, {
+    });
 
-  return html`
-<${React.Fragment}>
-</${React.Fragment}>
-`;
+    return html`
+        <${React.Fragment}>
+            <${If} condition=${ auth.user===false } key="105wvmxp">
+                Checking status ...
+            </${If}>
+            <${If} condition=${ auth.user===null } key="k0r0o5yd">
+            </${If}>
+            <${If} condition=${ auth.user } key="a9s3df61">
+            </${If}>
+        </${React.Fragment}>
+    `;
 };
 
 export default Module;
