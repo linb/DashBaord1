be15f85b6956df6736ced9771d0893b6b114b909
Jack Li <linb.net@gmail.com>
2020-12-06 21:59:44 -0500

    Updated by CrossUI React Builder

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ export default {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(mock(options));
-            }, 500);
+            }, 300);
         });        
     }
 };
