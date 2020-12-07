dcc2e08a058a34bb92bb4b7b61d64ef15c84335e
Jack Li <linb.net@gmail.com>
2020-12-06 21:35:09 -0500

    Updated by CrossUI React Builder

--- config.js
+++ config.js
@@ -1,3 +1,26 @@
+/*jshint esversion: 8 */
 export default {
-  baseUrl:"./mock_APIs/"
+    // baseUrl : "./mock_APIs/",
+    baseUrl : "",
+    mockFetch : (options) =>{
+        const mock = (options) => {
+            switch(options.url){
+                case "checkToken":
+                    break;
+                case "signIn":
+                    break;
+                case "signOut":
+                    break;
+                case "signUp":
+                    break;
+            }
+            return response;
+        };
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(mock(options));
+            }, 500);
+        });        
+    }
 };
