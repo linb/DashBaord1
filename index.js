3e711c0bed11aadc080e2faea191cc501b2ab64d
Jack Li <linb.net@gmail.com>
2020-12-08 01:55:50 -0500

    Updated by CrossUI React Builder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { React, html } from "./web_modules/preact-htm/index.js";
 import { ReactDOM } from "./web_modules/preact-htm/index.js";
 import { BrowserRouter, HashRouter as Router, RelativeRouter } from "./web_modules/react-hook-module/plugin_router.js";
 import { ProvideAuth } from "./web_modules/react-hook-module/plugin_auth.js";
-import { setGlobalBaseURL, setMockFetch } from "./web_modules/react-hook-module/plugin_request.js";
+import { setGlobalBaseURL, setFetchMocker } from "./web_modules/react-hook-module/plugin_request.js";
 import CONF from "./config.js";
 
 import App from "./pages/App.js";
